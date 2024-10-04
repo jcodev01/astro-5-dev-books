83d92abcb0ad9cad866b2607424feb9f07060cff
Jose Camacho <jcamachom3@est.ups.edu.ec>
2024-10-04 16:59:54 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+    }
+  }
+});
---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const books = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        img: z.string(),
+        readtime: z.number(),
+        buy: z.object({
+            spain: z.string().url(),
+            usa: z.string().url()
+        }),
+        description: z.string(),
+    })
+})
+
+export const collections = {
+    books
+}
